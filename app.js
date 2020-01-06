64b4ec8c05715c4bc2b1d09b0c2cef5cbf19886a
Philipp Dormann <philipp.dormann1@gmail.com>
2020-01-06 17:26:54 +0100

    working NodeJS scraper

--- app.js
+++ app.js
@@ -1,33 +1,21 @@
-/*
-start express web server on port 3000
-post request to https://mensadigital.de/LOGINPLAN.ASPX?P=FO111&E=herz
-print the response to the screen
-*/
 var express = require('express');
 var app = express();
-var request = require('request');
-
-var options = {
-	method: 'POST',
-	followAllRedirects: true,
-	url: 'https://mensadigital.de/LOGINPLAN.ASPX',
-	qs: { P: 'FO111', E: 'herz' },
-	headers: { 'content-type': 'multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW' },
-	formData: {
-		__VIEWSTATE:
-			'/wEPDwUJODQ5MDQ3NzI3D2QWAmYPZBYEAgEPDxYCHgRUZXh0BQVGTzExMWRkAgIPDxYCHwAFBGhlcnpkZGS0fKrirWn0k/UdgWunNZPR8jF3ShBdJIWVB0yGj1GA2w==',
-		__VIEWSTATEGENERATOR: 'D5B5CA0F',
-		btnLogin: ''
-	}
-};
+const puppeteer = require('puppeteer');
 
 app.get('/', function(req, res) {
-	request(options, function(error, response, body) {
-		if (error) throw new Error(error);
-		res.send(body);
-	});
+	async function loadPage() {
+		const browser = await puppeteer.launch();
+		const page = await browser.newPage();
+		await page.goto('https://mensadigital.de/LOGINPLAN.ASPX?P=FO111&E=herz');
+		// await page.screenshot({ path: 'mensa01.png' });
+		let htmlSource = await page.evaluate(() => document.body.outerHTML);
+		res.send(htmlSource);
+		await browser.close();
+	}
+
+	loadPage();
 });
 
-app.listen(3000, function() {
-	console.log('app now listening on port 3000!');
+app.listen(80, function() {
+	console.log('app now listening on port 80!');
 });
