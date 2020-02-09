9501f18069e52a5ebbf1aa8429a8576e49a8ef44
Philipp Dormann <philipp.dormann1@gmail.com>
2020-02-09 17:39:38 +0100

    modularize, fix bugs
    closes #2

---
+++ api/mensaplan-parser.js
@@ -0,0 +1,162 @@
+const cheerio = require('cheerio');
+let minify = require('html-minifier').minify;
+exports.Mensaplan_Parser = {
+	server_data: {},
+	parse: (html) => {
+		let $ = cheerio.load(html);
+		$('[style*="color:red"]').remove();
+		$('[style*="width:30%"]').remove();
+		$('[style*="border-spacing:0;padding:0"]').remove();
+		$('#lblMeineBestellungen').remove();
+		$('#lblLegende1').remove();
+		$('#lblSpeiesplanHinweis').remove();
+		$('#btnHideImage').remove();
+		$('#strDetails').remove();
+
+		$('sub').remove();
+		let mensaplan_info = $('#lblWoche').html();
+
+		parsed = $.html();
+		parsed = $('table#tblMain').html();
+		parsed = minify(parsed, {
+			useShortDoctype: true,
+			minifyCSS: true,
+			collapseWhitespace: true
+		});
+
+		parsed = parsed.replace(/ class="tdFooter3" style="height:auto"/g, '');
+		parsed = parsed.replace(
+			/ cellpadding="1" border="0" style="height:100%;width:100%;border-collapse:collapse;padding:0"/g,
+			''
+		);
+		parsed = parsed.replace(/ class="tdFooter3"/g, '');
+		parsed = parsed.replace(/ valign="top" colspan="3"/g, '');
+		parsed = parsed.replace(/ colspan="2" style="height:5px"/g, '');
+		parsed = parsed.replace(/ style="width:100%;overflow:hidden"/g, '');
+		parsed = parsed.replace(/padding:0"><div>[\d]+ /g, 'padding:0"><div>');
+		parsed = parsed.replace(/ valign="top" style="border-spacing:0;padding:0"/g, '');
+		parsed = parsed.replace(/ cellpadding="1" border="0" style="width:99%"/g, '');
+		parsed = parsed.replace(/ style="height:45px"/g, '');
+		parsed = parsed.replace(
+			/<tr class="tdFooter" style="background-color:#d0e0ff"><td style="font-weight:700">/g,
+			'<category>'
+		);
+		parsed = parsed.replace(/<\/td><td id="td0/g, '</category><td id="td0');
+		parsed = parsed.replace(
+			/<tr class="tdFooter2" style="background-color:#d0e0ff"><td style="font-weight:700">/g,
+			'<category>'
+		);
+
+		parsed = parsed.replace(/<th style="width:10%;background-color:#f0f0ff"><\/th>/g, '');
+		parsed = parsed.replace(/<\/th><th align="center" style="width:100px">/g, '</title-item><title-item>');
+		parsed = parsed.replace(/<\/th><\/tr>/g, '</title-item></title>');
+		parsed = parsed.replace(/<tr class="tdHeader"><th align="center" style="width:100px">/g, '<title><title-item>');
+		parsed = parsed.replace(/ [\d]+<\/div>/g, '</div>'); // Wackelpudding 002, 003 'n' stuff
+		parsed = parsed.replace(/<tr class="tdFooter">(?:.*)<\/tr><\/tbody>/g, '</tbody>'); //remove footer
+
+		//
+		parsed = parsed.replace(/ style="border-collapse:collapse;padding:0"/g, '');
+		parsed = parsed.replace(/ (id=("[^"]*"|))/g, ''); //remove IDs
+		//
+		parsed = parsed.replace(/<td>&#x2022;&#xA0;<\/td>/g, '');
+		parsed = parsed.replace(/<td align="center" valign="middle"><\/td>/g, '<food></food>');
+		parsed = parsed.replace(
+			/<\/div><\/td><\/tr><\/tbody><\/table><\/td><\/tr><tr><\/tr><\/tbody><\/table><\/td><food><\/food><td><table><tbody><tr><td><table><tbody><tr><td><\/td><\/tr><tr><td><div>/g,
+			'</food></day><day><food></food></day><day><food>'
+		);
+		parsed = parsed.replace(
+			/<\/div><\/td><\/tr><\/tbody><\/table><\/td><\/tr><tr><\/tr><\/tbody><\/table><\/td>/g,
+			'</food>'
+		);
+		parsed = parsed.replace(
+			/<\/category><td><table><tbody><tr><td><table><tbody><tr><td><\/td><\/tr><tr><td><div>/g,
+			'</category><day><food>'
+		);
+		parsed = parsed.replace(
+			/<\/div><\/td><\/tr><\/tbody><\/table><\/td><\/tr><tr><\/tr><\/tbody><\/table><\/td><\/tr>/g,
+			'</food></day>'
+		);
+		//
+		parsed = parsed.replace(
+			/<\/div><\/td><\/tr><\/tbody><\/table><\/td><\/tr><tr><\/tr><\/tbody><\/table><\/td><td><table><tbody><tr><td><table><tbody><tr><td><\/td><\/tr><tr><td><div>/g,
+			'</food></day><day><food>'
+		);
+		// multiple food per category in day
+		parsed = parsed.replace(/<\/div><\/td><\/tr><tr><td><div>/g, '</food><food>');
+		// next day
+		parsed = parsed.replace(
+			/<td><table><tbody><tr><td><table><tbody><tr><td><\/td><\/tr><tr><td><div>/g,
+			'</day><day><food>'
+		);
+
+		parsed = parsed.replace(/<\/tr>/g, '</day>'); // day closing
+
+		parsed = parsed.replace(/<\/td>/g, '</category>'); // last buggy td tag
+
+		parsed = parsed.replace(/<tbody>/g, ''); // tbody
+
+		parsed = this.Mensaplan_Parser.encode_umlauts(parsed);
+		parsed = this.Mensaplan_Parser.to_json(parsed);
+
+		// append general infos about the plan
+		mensaplan_info = mensaplan_info.replace(/<b>/g, '');
+		mensaplan_info = mensaplan_info.replace(/<\/b>/g, '');
+		let mensaplan_details = mensaplan_info.split(' - ');
+		mensaplan_details.full = mensaplan_info;
+		mensaplan_details.start = mensaplan_details[0];
+		mensaplan_info_modified = mensaplan_info.replace(/ \(KW(?:.*)/g, '');
+		mensaplan_details.end = mensaplan_info_modified.split(' - ');
+		mensaplan_details.end = mensaplan_details.end[1];
+		mensaplan_details.kw = mensaplan_info.split(' (KW');
+		mensaplan_details.kw = mensaplan_details.kw[1];
+		mensaplan_details.kw = mensaplan_details.kw.replace(/\)/g, '');
+
+		let appended_info =
+			'"full":"' +
+			mensaplan_details.full +
+			'","start":"' +
+			mensaplan_details.start +
+			'","end":"' +
+			mensaplan_details.end +
+			'","kw":"' +
+			mensaplan_details.kw +
+			'","server_data":' +
+			JSON.stringify(this.Mensaplan_Parser.server_data) +
+			'}';
+
+		parsed = parsed + `,"info":{${appended_info}}`;
+		return parsed;
+	},
+	to_json: (parsed) => {
+		parsed = parsed.replace(/<title><title-item>/g, '{"titles":["');
+		parsed = parsed.replace(/<\/title-item><title-item>/g, '","');
+		parsed = parsed.replace(/<\/title-item><\/title>/g, '"],"list":[');
+		parsed = parsed.replace(/<category>/g, '{"category":"');
+		parsed = parsed.replace(/<\/category>/g, '","days":[[');
+		parsed = parsed.replace(/<\/food><food>/g, '","');
+		parsed = parsed.replace(/<food>/g, '"');
+		parsed = parsed.replace(/<\/food>/g, '"');
+		parsed = parsed.replace(/<category>/g, '"category":"');
+		parsed = parsed.replace(/<\/day><day>/g, '],[');
+		parsed = parsed.replace(/<day>/g, '');
+		parsed = parsed.replace(/<\/day><\/tbody>/g, ']]}]');
+		// parsed = parsed.replace(/<\/day><\/tbody>/g, ']]}]}');
+		parsed = parsed.replace(/<\/day>/g, ']]},');
+
+		return parsed;
+	},
+	encode_umlauts: (parsed) => {
+		parsed = parsed.replace(/&#xE4;/g, 'ä');
+		parsed = parsed.replace(/&#xC4;/g, 'Ä');
+		parsed = parsed.replace(/&#xF6;/g, 'ö');
+		parsed = parsed.replace(/&#xD6;/g, 'Ö');
+		parsed = parsed.replace(/&#xFC;/g, 'ü');
+		parsed = parsed.replace(/&#xDC;/g, 'Ü');
+		parsed = parsed.replace(/&#xDF;/g, 'ß');
+		parsed = parsed.replace(/&#x1E9E;/g, 'ß');
+		parsed = parsed.replace(/&#xE9;/g, 'é');
+		parsed = parsed.replace(/&#xE8;/g, 'è');
+		parsed = parsed.replace(/&quot;/g, "'");
+		return parsed;
+	}
+};
--- parsing.js
+++ parsing.js
@@ -1,160 +1,14 @@
 const fs = require('fs');
-const cheerio = require('cheerio');
-let minify = require('html-minifier').minify;
+const mensaplan_parser = require('./api/mensaplan-parser');
 
 fs.readFile('test.html', function(err, data) {
 	if (err) return console.error(err);
 	let html = data.toString();
-	let $ = cheerio.load(html);
-	$('[style*="color:red"]').remove();
-	$('[style*="width:30%"]').remove();
-	$('[style*="border-spacing:0;padding:0"]').remove();
-	$('#lblMeineBestellungen').remove();
-	$('#lblLegende1').remove();
-	$('#lblSpeiesplanHinweis').remove();
-	$('#btnHideImage').remove();
-	$('#strDetails').remove();
 
-	$('sub').remove();
-	let mensaplan_info = $('#lblWoche').html();
-
-	parsed = $.html();
-	parsed = $('table#tblMain').html();
-	parsed = minifyHTML(parsed); //minify
-
-	// remove unusable styles + attrs
-	parsed = parsed.replace(
-		/ cellpadding="1" border="0" style="height:100%;width:100%;border-collapse:collapse;padding:0"/g,
-		''
-	);
-	parsed = parsed.replace(/ cellpadding="1" border="0" style="width:99%"/g, '');
-	parsed = parsed.replace(/ style="width:100%;overflow:hidden"/g, '');
-	parsed = parsed.replace(/ style="border-collapse:collapse;padding:0"/g, '');
-	parsed = parsed.replace(/ class="tdFooter2" style="background-color:#d0e0ff"/g, '');
-	parsed = parsed.replace(/ class="tdFooter3" style="height:45px"/g, ' ');
-	parsed = parsed.replace(/ style="font-weight:700"/g, '');
-	// 3758 Grießbrei 'n' stuff
-	parsed = parsed.replace(/padding:0"><div>[\d]+/g, 'padding:0"><div>');
-	parsed = parsed.replace(/padding:0"><div> /g, 'padding:0"><div>');
-	// Wackelpudding 002 'n' stuff
-	parsed = parsed.replace(/ [\d]+<\/div>/g, '</div>');
-	// more
-	parsed = parsed.replace(/ valign="top" style="border-spacing:0;padding:0"/g, '');
-	parsed = parsed.replace(/ class="tdFooter3"/g, '');
-	parsed = parsed.replace(/ class="tdFooter" style="background-color:#d0e0ff"/g, ' ');
-	parsed = parsed.replace(/<tr style="height:auto"><td valign="top" colspan="3">/g, '<tr><td>');
-	parsed = parsed.replace(/ align="center" valign="middle"/g, '');
-	parsed = parsed.replace(/ colspan="2" style="height:5px"/g, '');
-	parsed = parsed.replace(/ align="center" style="width:100px"/g, '');
-	parsed = parsed.replace(/<tr class="tdHeader"><th style="width:10%;background-color:#f0f0ff"><\/th>/g, '<title>');
-	parsed = parsed.replace(/<tr class="tdFooter">(?:.*)<\/tr><\/tbody>/g, '</tbody>'); //remove footer
-	parsed = parsed.replace(/ >/g, '>'); // remove weird whitespace in "<tr >"
-	// remove last IDs
-	parsed = parsed.replace(/<td id="td[\d]+_[\d]+">/g, '<td>');
-
-	// JS encoded umlauts
-	parsed = parsed.replace(/&#xE4;/g, 'ä');
-	parsed = parsed.replace(/&#xC4;/g, 'Ä');
-	parsed = parsed.replace(/&#xF6;/g, 'ö');
-	parsed = parsed.replace(/&#xD6;/g, 'Ö');
-	parsed = parsed.replace(/&#xFC;/g, 'ü');
-	parsed = parsed.replace(/&#xDC;/g, 'Ü');
-	parsed = parsed.replace(/&#xDF;/g, 'ß');
-	parsed = parsed.replace(/&#x1E9E;/g, 'ß');
-	parsed = parsed.replace(/&#xE9;/g, 'é');
-	parsed = parsed.replace(/&#xE8;/g, 'è');
-	parsed = parsed.replace(/&quot;/g, "'"); //&quot;
-	parsed = parsed.replace(/&#x2022;&#xA0;/g, '');
-
-	//--- processing to useful stuff start
-	//*** title
-	parsed = parsed.replace(/<\/th><\/tr>/g, '</title>');
-	parsed = parsed.replace(/<th>/g, '<title-item>');
-	parsed = parsed.replace(/<\/th>/g, '</title-item>');
-	parsed = parsed.replace(/<\/title>/g, '</title-item></title>');
-
-	// more
-	parsed = parsed.replace(/<div>/g, '<essen>');
-	parsed = parsed.replace(/<\/div>/g, '</essen>');
-	// special case (Nachtisch - 3 filled, 1 empty)
-	parsed = parsed.replace(
-		/<\/td><\/tr><\/tbody><\/table><\/td><\/tr><tr><\/tr><\/tbody><\/table><\/td><td><\/td><\/tr><tr><td>/,
-		'</day><day><essen></essen></day><category>'
-	);
-	parsed = parsed.replace(
-		/<\/essen><\/td><\/tr><\/tbody><\/table><\/td><\/tr><tr><\/tr><\/tbody><\/table><\/td><td><table><tbody><tr><td><table><tbody><tr><td><\/td><\/tr><tr><td><\/td><td>/g,
-		'</essen></day><day>'
-	);
-	parsed = parsed.replace(
-		/<\/essen><\/td><\/tr><\/tbody><\/table><\/td><\/tr><tr><\/tr><\/tbody><\/table><\/td><td><\/td><\/tr><tr><td>/g,
-		'</essen></day><category>'
-	);
-	parsed = parsed.replace(
-		/<\/td><td><table><tbody><tr><td><table><tbody><tr><td><\/td><\/tr><tr><td><\/td><td><essen>/g,
-		'</category><day><essen>'
-	);
-	parsed = parsed.replace(/<\/essen><\/td><\/tr><tr><td><\/td><td><essen>/g, '</essen><essen>');
-	parsed = parsed.replace(
-		/<\/td><\/tr><\/tbody><\/table><\/td><\/tr><tr><\/tr><\/tbody><\/table><\/td><\/tr><tr><td>/g,
-		'</day><category>'
-	);
-	// special case (Nachtisch alternativ - 3 empty, 1 filled)
-	parsed = parsed.replace(
-		/<\/td><td><\/td><td><\/td><td><\/category>/g,
-		'</category><day><essen></essen></day><day><essen></essen></day><day><essen></essen></day>'
-	);
-	// first element
-	parsed = parsed.replace(/<tr><td>/g, '<category>');
-	// last element
-	parsed = parsed.replace(
-		/<\/essen><\/td><\/tr><\/tbody><\/table><\/td><\/tr><tr><\/tr><\/tbody><\/table><\/td><\/tr><\/tbody>/g,
-		'</essen></day><END>'
-	);
-
-	//--- to JSON
-	parsed = parsed.replace(/<\/title-item><\/title>/g, '"],"list":[');
-	parsed = parsed.replace(/<\/title-item><title-item>/g, '","');
-	parsed = parsed.replace(/<tbody><title><title-item>/g, '{"titles":["');
-	parsed = parsed.replace(/<category>/g, '{"category":"');
-	parsed = parsed.replace(/<\/category>/g, '","days":[');
-	// parsed = parsed.replace(/<day>/g, '{');
-	parsed = parsed.replace(/<\/essen><\/day><day><essen>/g, '"],["');
-	parsed = parsed.replace(/<day><essen>/g, '["');
-	parsed = parsed.replace(/<\/essen><essen>/g, '","');
-	parsed = parsed.replace(/<\/essen><\/day>/g, '"]]},');
-
-	// append general infos about the plan
-	mensaplan_info = mensaplan_info.replace(/<b>/g, '');
-	mensaplan_info = mensaplan_info.replace(/<\/b>/g, '');
-	let mensaplan_details = mensaplan_info.split(' - ');
-	mensaplan_details.full = mensaplan_info;
-	mensaplan_details.start = mensaplan_details[0];
-	mensaplan_info_modified = mensaplan_info.replace(/ \(KW(?:.*)/g, '');
-	mensaplan_details.end = mensaplan_info_modified.split(' - ');
-	mensaplan_details.end = mensaplan_details.end[1];
-	mensaplan_details.kw = mensaplan_info.split(' (KW');
-	mensaplan_details.kw = mensaplan_details.kw[1];
-	mensaplan_details.kw = mensaplan_details.kw.replace(/\)/g, '');
-
-	let server_data = {};
-	server_data = JSON.stringify(server_data);
-
-	let appended_info =
-		'],"full":"' +
-		mensaplan_details.full +
-		'","start":"' +
-		mensaplan_details.start +
-		'","end":"' +
-		mensaplan_details.end +
-		'","kw":"' +
-		mensaplan_details.kw +
-		'","server_data":' +
-		server_data +
-		'}';
-
-	parsed = parsed.replace(/,<END>/g, appended_info);
+	let parsed = mensaplan_parser.Mensaplan_Parser.parse(html);
 
 	log_it(parsed, 'parsed.json');
+	log_it(parsed, 'parsed.html');
 });
 
 let log_it = (data, file) => {
@@ -163,16 +17,5 @@ let log_it = (data, file) => {
 			return console.log(err);
 		}
 		console.log(`The file ${file} was saved!`);
-	});
-};
-
-let minifyHTML = (html) => {
-	return minify(html, {
-		useShortDoctype: true,
-		minifyCSS: true,
-		html5: true,
-		collapseWhitespace: true,
-		removeComments: true,
-		removeEmptyAttributes: true
 	});
 };
