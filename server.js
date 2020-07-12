891d7826593baa42019c7321e3f8f70225684d1b
Philipp Dormann <philipp.dormann1@gmail.com>
2020-07-12 21:57:50 +0200

    default to project readme?

--- server.js
+++ server.js
@@ -2,15 +2,14 @@ const express = require('express');
 const start_it_up = require('./api/index');
 const app = express();
 
-app.use('/docs', (req, res) => {
-	// return res.redirect('https://github.com/philippd1/gymhmensa');
-	return res.sendFile('./index.html', { root: './' });
-});
-app.use('/list', (req, res) => {
+app.use(['/list', '/institutes'], (req, res) => {
 	return res.send();
 });
-app.use('*', (req, res) => {
+app.use('/api', (req, res) => {
 	return start_it_up(req, res);
+});
+app.use('*', (req, res) => {
+	return res.sendFile('./index.html', { root: './' });
 });
 
 app.listen(80, () => console.log('listening on port 80!'));
