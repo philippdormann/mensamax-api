09f8f74fee5a3244057d577841b31e8024df5768
Philipp Dormann <philipp.dormann1@gmail.com>
2020-07-13 10:28:43 +0200

    more Vercel improvements/ trials

--- page-home-ui.js
+++ page-home-ui.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const templateData = fs.readFileSync('./web-template.html', 'utf-8');
-const content = fs.readFileSync('./index.html', 'utf-8');
+const content = fs.readFileSync('./home.html', 'utf-8');
 module.exports = (req, res) => {
 	const rendered = templateData.replace(/{{{content}}}/gi, content);
 	return res.send(rendered);
