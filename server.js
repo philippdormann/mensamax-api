70170bbcb8938bbe5c23b3d5b7ba0c343511401d
Philipp Dormann <philipp.dormann1@gmail.com>
2020-07-13 10:06:09 +0200

    make it work without Vercel

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const port = process.env.PORT || 80;
 app.use('/institutions-json', (req, res) =>
 	res.sendFile('./institutions.json', { root: './' })
 );
-app.use('/institutions-ui', (req, res) => institutionsPage('./', req, res));
+app.use('/institutions-ui', (req, res) => institutionsPage(req, res));
 app.use('/api', (req, res) => main(req, res));
-app.use('*', (req, res) => homePage('./', req, res));
+app.use('*', (req, res) => homePage(req, res));
 
 app.listen(port, () => console.log(`listening on port ${port}!`));
