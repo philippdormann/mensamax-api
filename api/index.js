d09d8956fb895988f2b532b675e6410facf4f0a0
Philipp Dormann <philipp@philippdormann.de>
2021-06-25 00:56:54 +0200

    🧹 cleanup
    ref #10

--- api/index.js
+++ api/index.js
@@ -1,64 +1,22 @@
-const mensaplan_parser = require('./parser');
-const fs = require('fs');
-const { join } = require('path');
-let institutions = JSON.parse(
-	fs.readFileSync(join(__dirname, `../institutions.json`), 'utf-8')
-);
-const start_it_up = (req, res) => {
-	console.log(req.query);
-	if (req.query.p && req.query.e) {
-		let found = institutions.find(function (e) {
-			return e.project == req.query.p && e.facility == req.query.e;
-		});
-		if (
-			found.provider &&
-			found.verified__VIEWSTATEGENERATOR &&
-			found.tested === true
-		) {
-			request(
-				{
-					followAllRedirects: true,
-					method: 'POST',
-					url: `https://${found.provider}/LOGINPLAN.ASPX`,
-					qs: { P: req.query.p, E: req.query.e },
-					formData: {
-						// TODO: we need to handle the viewstate.
-						__VIEWSTATE: found.verified__VIEWSTATE,
-						__VIEWSTATEGENERATOR:
-							found.verified__VIEWSTATEGENERATOR,
-						btnLogin: ''
-					}
-				},
-				(error, response, body) => {
-					if (error) {
-						console.log(error);
-						return send_it(
-							500,
-							{ status: 'fail', payload: error },
-							req,
-							res
-						);
-					}
-					const parsed = JSON.parse(
-						mensaplan_parser.Mensaplan_Parser.parse(body)
-					);
-					return send_it(
-						200,
-						{ status: 'ok', payload: parsed },
-						req,
-						res
-					);
-				}
-			);
-		} else {
-			// TODO: more error handling
-			res.send(
-				`https://${found.provider}/LOGINPLAN.ASPX?p=${req.query.p}&e=${req.query.e}`
-			);
-		}
-		console.log(found);
-	} else {
-		// TODO: handle missing params: return ../institutions.json ?
+const { fetcher } = require('./fetcher');
+const { parser } = require('./parser');
+module.exports = async function start_it_up(req, res) {
+	try {
+		const out = await fetcher({ p: req.query.p, e: req.query.e })
+		const parsed = await parser(out)
+		return send_it(
+			200,
+			{ status: 'ok', payload: parsed },
+			req,
+			res
+		);
+	} catch (e) {
+		return send_it(
+			500,
+			{ status: 'fail', payload: e },
+			req,
+			res
+		);
 	}
 };
 
@@ -68,5 +26,3 @@ const send_it = (status, body, req, res) => {
 	res.setHeader('Content-Type', 'application/json');
 	res.status(status).send(body);
 };
-
-module.exports = start_it_up;
