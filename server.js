d4f1d5138f5c76aadca7d647cb5e055834c736c5
Philipp Dormann <philipp@philippdormann.de>
2021-06-26 00:09:58 +0200

    🔌 add dotenv for port defaulting

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const main = require('./api/index');
 const institutionsPage = require('./page-institutions-ui');
 const homePage = require('./page-home-ui');
 const app = express();
+require('dotenv').config();
 const port = process.env.PORT || 80;
 
 app.use('/institutions-json', (req, res) =>
