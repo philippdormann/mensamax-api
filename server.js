1c8c4a095db88fdd404a22e1ec3895a2aedeb681
Philipp Dormann <philipp@philippdormann.de>
2022-12-30 12:21:07 +0100

    update server.js for new ESM rayo import

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
-const rayo = require('rayo');
-const main = require('./api/index');
-const institutionsPage = require('./page-institutions-ui');
-const homePage = require('./page-home-ui');
-require('dotenv').config();
-const port = process.env.PORT || 80;
-const app = rayo({ port });
-const institutions = require('./institutions.json');
-const institutionsString = JSON.stringify(institutions);
+(async () => {
+	const rayo = await (await import('rayo')).default;
+	const main = require('./api/index');
+	const institutionsPage = require('./page-institutions-ui');
+	const homePage = require('./page-home-ui');
+	require('dotenv').config();
+	const port = process.env.PORT || 80;
+	const app = rayo({ port });
+	const institutions = require('./institutions.json');
+	const institutionsString = JSON.stringify(institutions);
 
-app.all('/institutions-json', (req, res) => res.end(institutionsString));
-app.all('/institutions-ui', (req, res) => institutionsPage(req, res));
-app.all('/api', (req, res) => main(req, res));
-app.all('*', (req, res) => homePage(req, res));
+	app.all('/institutions-json', (req, res) => res.end(institutionsString));
+	app.all('/institutions-ui', (req, res) => institutionsPage(req, res));
+	app.all('/api', (req, res) => main(req, res));
+	app.all('*', (req, res) => homePage(req, res));
 
-app.start();
+	app.start();
+	console.log(`app listening on port ${port}`);
+})().catch((err) => console.error(err));
