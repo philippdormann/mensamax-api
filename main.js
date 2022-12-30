a393608f1f2ff343197b7f31fd0202e83f7d785f
Philipp Dormann <philipp@philippdormann.de>
2022-12-30 12:32:16 +0100

    export institutions from package

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const { fetcher } = require('./api/fetcher');
 const { parser } = require('./api/parser');
+const institutions = require('./institutions.json');
 //
 exports.fetcher = fetcher;
 exports.parser = parser;
+exports.institutions = institutions;
